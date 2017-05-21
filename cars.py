01f472beea45c72439a49a88319bdb73e260fdef
Peter Schüllermann <Peter.schuellermann>
2017-05-21 16:09:47 +0200

    add hog to feature extraction algorithm and add labels func

--- cars.py
+++ cars.py
@@ -3,12 +3,13 @@ import cv2
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 from sklearn.preprocessing import StandardScaler
+from skimage.feature import hog
 import glob
 
 
 def train():
-    global feature_map
-    feature_map = train_on_images()
+    global feature_map, feature_labels
+    feature_map, feature_labels = train_on_images()
 
 
 def detect(image):
@@ -37,10 +38,27 @@ def color_hist(img, nbins=32, bins_range=(0, 256)):
     return hist_features
 
 
+# Define a function to return HOG features and visualization
+def get_hog_features(img, orient, pix_per_cell, cell_per_block,
+                        vis=False, feature_vec=True):
+    # Call with two outputs if vis==True
+    if vis == True:
+        features, hog_image = hog(img, orientations=orient, pixels_per_cell=(pix_per_cell, pix_per_cell),
+                                  cells_per_block=(cell_per_block, cell_per_block), transform_sqrt=True,
+                                  visualise=vis, feature_vector=feature_vec)
+        return features, hog_image
+    # Otherwise call with one output
+    else:
+        features = hog(img, orientations=orient, pixels_per_cell=(pix_per_cell, pix_per_cell),
+                       cells_per_block=(cell_per_block, cell_per_block), transform_sqrt=True,
+                       visualise=vis, feature_vector=feature_vec)
+        return features
+
+
 # Define a function to extract features from a list of images
 # Have this function call bin_spatial() and color_hist()
-def extract_features(imgs, cspace='RGB', spatial_size=(32, 32),
-                     hist_bins=32, hist_range=(0, 256)):
+def extract_features(imgs, hog_data, cspace='RGB', spatial_size=(32, 32),
+                     hist_bins=32, hist_range=(0, 256),):
     # Create a list to append feature vectors to
     features = []
 
@@ -60,12 +78,23 @@ def extract_features(imgs, cspace='RGB', spatial_size=(32, 32),
                 feature_image = cv2.cvtColor(image, cv2.COLOR_RGB2YUV)
         else:
             feature_image = np.copy(image)
+
+        # Call get_hog_features() with vis=False, feature_vec=True
+        hog_features = []
+        for channel in range(feature_image.shape[2]):
+            hog_features.append(get_hog_features(feature_image[:,:,channel],
+                                                 hog_data["orient"],
+                                                 hog_data["pix_per_cell"],
+                                                 hog_data["cell_per_block"],
+                                vis=False, feature_vec=True))
+        hog_features = np.ravel(hog_features)
+
         # Apply bin_spatial() to get spatial color features
         spatial_features = bin_spatial(feature_image, size=spatial_size)
         # Apply color_hist() also with a color space option now
         hist_features = color_hist(feature_image, nbins=hist_bins, bins_range=hist_range)
         # Append the new feature vector to the features list
-        features.append(np.concatenate((spatial_features, hist_features)))
+        features.append(np.concatenate((spatial_features, hist_features, hog_features)))
     # Return list of feature vectors
     return features
 
@@ -83,10 +112,11 @@ def train_on_images():
     notcars = []
     for notcar_image in images_notcars:
         notcars.append(notcar_image)
+    hog_data = {"orient":9, "pix_per_cell": 8, "cell_per_block": 2}
 
-    car_features = extract_features(cars, cspace='RGB', spatial_size=(32, 32),
+    car_features = extract_features(cars, hog_data, cspace='RGB', spatial_size=(32, 32),
                                     hist_bins=32, hist_range=(0, 256))
-    notcar_features = extract_features(notcars, cspace='RGB', spatial_size=(32, 32),
+    notcar_features = extract_features(notcars, hog_data, cspace='RGB', spatial_size=(32, 32),
                                        hist_bins=32, hist_range=(0, 256))
 
     if len(car_features) > 0:
@@ -96,6 +126,7 @@ def train_on_images():
         X_scaler = StandardScaler().fit(X)
         # Apply the scaler to X
         scaled_X = X_scaler.transform(X)
-        return scaled_X
+        y = np.hstack((np.ones(len(car_features)), np.zeros(len(notcar_features))))
+        return scaled_X, y
     else:
         print('Your training function only returns empty feature vectors...')
